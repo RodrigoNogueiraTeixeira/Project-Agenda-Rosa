e689ad7863515b750280f3da987148c0cefecb56
DaniielOliveira-Dev <danieldeoliveira03.10@gmail.com>
2026-06-08 14:16:40 -0300

    refactor: simplifica perfil do estabelecimento

--- backend/src/empresa/repositories/empresaRepository.js
+++ backend/src/empresa/repositories/empresaRepository.js
@@ -1,6 +1,7 @@
 const empresaDAO = require("../dao/empresaDAO");
 const passwordUtils = require("../../utils/password");
 
+// Cadastra a empresa somente se o e-mail estiver disponivel.
 async function cadastrarEmpresa(dados) {
   const empresaExistente = await empresaDAO.buscarPorEmail(dados.email);
 
@@ -23,15 +24,21 @@ async function buscarPerfil(empresaId) {
   return empresaDAO.buscarPerfil(empresaId);
 }
 
+// Confere o e-mail antes de atualizar o perfil.
 async function atualizarPerfil(dados) {
   const empresaComEmail = await empresaDAO.buscarPorEmail(dados.email);
 
-  if (empresaComEmail && Number(empresaComEmail.id) !== Number(dados.empresaId)) {
+  if (
+    empresaComEmail &&
+    Number(empresaComEmail.id) !== Number(dados.empresaId)
+  ) {
     throw new Error("E-mail ja cadastrado por outra empresa.");
   }
 
   await empresaDAO.atualizarPerfil(dados);
-  return empresaDAO.buscarPerfil(dados.empresaId);
+  const perfilAtualizado = await empresaDAO.buscarPerfil(dados.empresaId);
+
+  return perfilAtualizado;
 }
 
 module.exports = {
